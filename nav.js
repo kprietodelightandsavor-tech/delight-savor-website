229d2646bc46333efbcb1f99c20acb1a41fd0b57
kprietodelightandsavor-tech <kprieto.delightandsavor@gmail.com>
2026-06-16 11:24:55 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -47,10 +47,11 @@
 </div>`;
  /* ── INJECT FAVICON + APPLE TOUCH ICON ── */
 [
-  { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/images/favicon-32x32.png' },
-  { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/images/favicon-16x16.png' },
-  { rel: 'icon', href: '/images/favicon.ico', sizes: 'any' },
-  { rel: 'apple-touch-icon', sizes: '180x180', href: '/images/apple-touch-icon.png' }
+  { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/images/favicon-96x96.png' },
+  { rel: 'icon', type: 'image/svg+xml', href: '/images/favicon.svg' },
+  { rel: 'shortcut icon', href: '/images/favicon.ico' },
+  { rel: 'apple-touch-icon', sizes: '180x180', href: '/images/apple-touch-icon.png' },
+  { rel: 'manifest', href: '/images/site.webmanifest' }
 ].forEach(function (i) {
   const link = document.createElement('link');
   Object.keys(i).forEach(function (k) { link.setAttribute(k, i[k]); });
